9704efdfdbfb03e9c268e9fff890038143b3f04e
tai phan <phancongtai2621998@gmail.com>
2020-08-03 20:52:02 +0700

    application_layer

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,27 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{    // interface cho admin
+    public interface IManageProductService
+    {   // định nghĩa các phương thức
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequets request);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+        Task AddViewcount(int prodductId);
+
+        Task<int> Delete(int productId);
+
+        Task<List<ProductViewModel>> Getall();
+        Task<PagedViewModel<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+    }
+}
---
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -0,0 +1,71 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public class ManageProductService : IManageProductService
+    {   
+        private readonly EShopDbContext _context; // đọc 1 lần
+        public ManageProductService(EShopDbContext context) 
+        {
+            _context = context;
+        }
+
+        public Task AddViewcount(int prodductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> Create(ProductCreateRequest request)
+        {
+            var product = new Product() //Entities
+            {
+                Price = request.Price,
+            };
+            _context.Products.Add(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductViewModel>> Getall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedViewModel<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> Update(ProductUpdateRequets requets)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePrice(int productId, decimal newPrice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateStock(int productId, int addedQuantity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
